5c239aa819e22a9cac5cf1861f02056aacfbc921
Elliot Schrock <eschrock@mobiquityinc.com>
2014-01-10 20:24:38 -0500

    alice's changes

--- QueuerDemo/app/src/main/java/com/demo/queuer/LoginActivity.java
+++ QueuerDemo/app/src/main/java/com/demo/queuer/LoginActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Toast;
 
 public class LoginActivity extends ActionBarActivity {
 
@@ -23,27 +24,6 @@ public class LoginActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     /**
