9a51a83c215caa5b80ce2130ce02573cd9cf8622
Elliot Schrock <eschrock@mobiquityinc.com>
2014-01-10 22:11:52 -0500

    alice's changes

--- QueuerDemo/app/src/main/java/com/demo/queuer/LoginActivity.java
+++ QueuerDemo/app/src/main/java/com/demo/queuer/LoginActivity.java
@@ -1,16 +1,7 @@
 package com.demo.queuer;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.Toast;
 
 public class LoginActivity extends ActionBarActivity {
 
@@ -18,28 +9,6 @@ public class LoginActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_login, container, false);
-            return rootView;
-        }
     }
 
 }
